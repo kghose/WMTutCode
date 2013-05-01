8b5cdc6e0844792d5c94e2fcd28ee633dfc5dea8
Kaushik Ghose <kaushik.ghose@gmail.com>
2013-05-01 14:21:19 -0400

    Code and documentation clean up

--- corsiblocks.py
+++ corsiblocks.py
@@ -2,28 +2,24 @@
 We use a class to implement the game because we need to carry around a bunch of metadata that things like onlick
 need to access.
 
-The display of the corsi blocks occurs within a single method run_corsi which reads in the current difficulty level and then generates a sequence of blocks which it then proceeds to display. At the end of the display it sets up a simple sequence counter that points to which corsi block we are at (the 1st one) and waits for mouse presses on the main screen. Pressing any of the menu items aborts the trial just displayed and executes the menu item. Clicking on the main screen tests to see if the click is on an appropriate block. If it is not, feedback is given and the next trial is started. If the block is appropriate then we move the counter along until all blocks are done, at which point we give feed back and start the next trial.
+The program generates a sequence of blocks and then displays them. At the end of the display it sets up a simple sequence counter that points to which corsi block we are at (the 1st one) and waits for mouse presses on the main screen. Pressing any of the menu items aborts the trial just displayed and executes the menu item. Clicking on the main screen tests to see if the click is on an appropriate block. If it is not, feedback is given and the next trial is started. If the block is appropriate then we move the counter along until all blocks are done, at which point we give feed back and start the next trial.
 
 To generate app on mac
 
 python ~/bin/pyinstaller-2.0/pyinstaller.py -F -w -y corsiblocks.py
 
-TODO: Implement staircase method and give realtime feedback at bottom as a strip chart and current threshold value
 """
 import matplotlib
 matplotlib.use('macosx')
-import matplotlib.animation as animation
-import pylab, time
+import pylab
 
 class CorsiBlocks:
   def __init__(self):
     self.setup_main_screen()
-    self.ani = None
-    self.span = 2 #How many block
-    self.in_trial = False
-    self.showing_sequence = False
-    self.showing_period = False
-    self.streak = 0
+    self.span = 2 #How many blocks?
+    self.in_trial = False #Determines how to interpret clicks on the panel
+    self.showing_sequence = False #Are we in the middle of showing the blocks? Needed to abort trial if we move move into panel in the middle
+    self.streak = 0 #How many corrects have we got in a row?
     self.staircase_streak = 2 #How many corrects before we increase difficulty
     self.span_history = [] #Our trial results
 
@@ -38,7 +34,7 @@ class CorsiBlocks:
     self.ax = {}
     #Main corsi blocks display
     self.ax['main'] = pylab.subplot2grid((rows, cols), (0, 0), colspan=cols, rowspan=rows-1)
-    self.clear_main_screen('Move the mouse out of this panel to start\nA sequence of blocks will appear\nAt the end of the sequence\nThe panel will change color\nClick on the blocks in the order they appeared.')
+    self.clear_main_screen('Move the mouse out of this panel to start.\nA sequence of blocks will appear in this panel.\nAt the end of the sequence the panel will change color.\nAfter the color change, click on the blocks in the order they appeared.')
 
     #The staircase display with span estimate
     self.ax['staircase'] = pylab.subplot2grid((rows, cols), (9, 1), colspan=cols-1)
@@ -46,7 +42,6 @@ class CorsiBlocks:
     self.cid = self.fig.canvas.mpl_connect('button_press_event', self.onclick)
     self.cid2 = self.fig.canvas.mpl_connect('axes_leave_event', self.onleaveaxes)
     self.cid3 = self.fig.canvas.mpl_connect('axes_enter_event', self.onenteraxes)
-
 
   def clear_main_screen(self, msg=None):
     ax = self.ax['main']
@@ -58,20 +53,22 @@ class CorsiBlocks:
   def onleaveaxes(self, event):
     if event.inaxes == self.ax['main']:
       if not self.in_trial:
-        self.start_session()
+        self.start_trial()
 
   def onenteraxes(self, event):
     if event.inaxes == self.ax['main']:
       if self.showing_sequence:
-        self.abort_session()
+        self.abort_trial()
 
   def block_sequence(self):
+    """The creates a generator that will step us through the sequence of blocks we want to present."""
     for n in self.this_sequence:
       x = n%5
       y = n/5
       yield x,y
 
   def draw_sequence_frame(self):
+    """Adds a block to our main panel, using the iterator (self.bs_gen) obtained from block_sequence."""
     ax = self.ax['main']
     ax.patch.set_facecolor('w')
     try:
@@ -86,7 +83,8 @@ class CorsiBlocks:
       pylab.draw()
 
 
-  def start_session(self):
+  def start_trial(self):
+    """The only funky thing going on here is that we need to call self.block_sequence() to obtain a generator."""
     self.in_trial = True
     self.current_block = 0
     self.showing_sequence = True
@@ -101,7 +99,9 @@ class CorsiBlocks:
     self.clear_main_screen()
     self.event_source.start()
 
-  def abort_session(self):
+  def abort_trial(self):
+    """If we move our mouse into the arena prematurely this is called to start over. Nothing is added to the trial
+    record."""
     self.event_source.callbacks = []
     self.clear_main_screen('Wait for the screen to change color\nbefore starting your answer.\nMove mouse out of area to continue')
     ax = self.ax['main']
@@ -111,6 +111,7 @@ class CorsiBlocks:
     pylab.draw()
 
   def test_block(self, event):
+    """Tests if our click has fallen on the block it is supposed to fall on."""
     x = event.xdata
     y = event.ydata
     idx = self.this_sequence
@@ -126,6 +127,7 @@ class CorsiBlocks:
       self.trial_correct(False)
 
   def trial_correct(self, result):
+    """Updates the trial record and our strip chart plot based on the result of the trial."""
     if result:
       msg = 'Correct!\nMove mouse out of area to continue'
       self.streak += 1
@@ -159,7 +161,7 @@ class CorsiBlocks:
     if event.inaxes == self.ax['main']:#We clicked on main screen
       if self.in_trial:
         if self.showing_sequence:#test if our stim presentation is still running
-          #To be harsh, we should abort the trial, we just ignore it
+          #We should not get here
           return
         else: #We are in a trial, and have finished stim presentation
           self.test_block(event)
